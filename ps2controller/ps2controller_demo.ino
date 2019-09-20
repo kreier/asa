ea17c8eeaef69a01ebad178b1855a2ba1e157541
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-09-21 00:00:43 +0700

    indicate PWM pins

--- ps2controller/ps2controller_demo.ino
+++ ps2controller/ps2controller_demo.ino
@@ -6,9 +6,9 @@
  *   - 2e colmun: Stef?
  * replace pin numbers by the ones you use
  ******************************************************************/
-#define PS2_DAT        13  //14    
-#define PS2_CMD        11  //15
-#define PS2_SEL        10  //16
+#define PS2_DAT        13  //14    PWM
+#define PS2_CMD        11  //15    PWM
+#define PS2_SEL        10  //16    PWM
 #define PS2_CLK        12  //17
 
 /******************************************************************
