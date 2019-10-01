fbab7079713c1da9374ad066b531a3d2f68eccd9
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-10-01 15:09:47 +0700

    move to motorshield folder

---
+++ motorshield/motortest_simple-2.ino
@@ -0,0 +1,25 @@
+int E1 = 10; // enable - with PWM
+int M1 = 12;
+int E2 = 11;
+int M2 = 13;
+
+int speed = 0;
+
+void setup() {
+  pinMode(M1, OUTPUT); // no need for analog output
+  pinMode(M2, OUTPUT);
+  // enable
+  analogWrite(E1, 255);
+  analogWrite(E2, 255);   
+}
+
+void loop() {
+  // forward
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+  delay(1000);
+  // backward
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, LOW);
+  delay(1000);
+}
